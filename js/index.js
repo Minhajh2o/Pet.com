265b0eaae1bbddfc3591133779ec0157e396c0cc
Minhajh2o <h2o.minhaj@gmail.com>
2025-04-29 22:21:52 +0600

    header and main section added. buuttons loaded at main section using api

--- js/index.js
+++ js/index.js
@@ -1,8 +1,27 @@
-// burgerMenu.js
-document.addEventListener("DOMContentLoaded", () => {
-    const container = document.getElementById("burger-menu-container");
+const loadCategories =()=>{
+    fetch('https://openapi.programming-hero.com/api/peddy/categories')
+    .then(response => response.json())
+    .then(data => loadCategoryButtons(data.categories))
+    .catch(error => console.error('Error fetching data:', error));
+}
 
-    container.innerHTML = `
-        
-    `;
-});
+const loadCategoryButtons = (categories) => {
+    // console.log(categories);
+    const categoryBtnContainer = document.getElementById('category-btn-container');
+    categories.forEach(item => {
+        const categoryContainer = document.createElement('div');
+        categoryContainer.innerHTML = `
+            <a id="btn-${item.id}" onclick="loadCategoryVideos(${item.id})" class="btn flex items-center gap-2 px-4 py-2 rounded-lg">
+            <img src="${item.category_icon}" alt="${item.category}" class="w-6 h-6 ">
+            <p>${item.category}</p>
+            </a>
+        `;
+        categoryBtnContainer.append(categoryContainer);
+    });
+}
+
+const loadCategoryVideos = (categoryId) => {
+    console.log(categoryId);
+}
+
+loadCategories();
