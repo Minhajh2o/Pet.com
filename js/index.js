22052b5c204e66dfdb8f3009eb2b9730f51c0aea
Minhajh2o <h2o.minhaj@gmail.com>
2025-04-30 23:12:49 +0600

    like functionality added.

--- js/index.js
+++ js/index.js
@@ -1,49 +1,51 @@
-const loadCategories =()=>{
-    fetch('https://openapi.programming-hero.com/api/peddy/categories')
-    .then(response => response.json())
-    .then(data => loadCategoryButtons(data.categories))
-    .catch(error => console.error('Error fetching data:', error));
-}
+const loadCategories = () => {
+  fetch("https://openapi.programming-hero.com/api/peddy/categories")
+    .then((response) => response.json())
+    .then((data) => loadCategoryButtons(data.categories))
+    .catch((error) => console.error("Error fetching data:", error));
+};
 
 // load Category Buttons
 const loadCategoryButtons = (categories) => {
-    // console.log(categories);
-    const categoryBtnContainer = document.getElementById('category-btn-container');
-    categories.forEach(item => {
-        const categoryContainer = document.createElement('div');
-        categoryContainer.innerHTML = `
+  // console.log(categories);
+  const categoryBtnContainer = document.getElementById(
+    "category-btn-container"
+  );
+  categories.forEach((item) => {
+    const categoryContainer = document.createElement("div");
+    categoryContainer.innerHTML = `
             <a id="category-${item.id}" onclick="loadActiveBtn(${item.id})" class="flex items-center justify-center gap-2 md:gap-3 px-4 py-2 md:w-[168px] lg:w-[280px] rounded-lg border-2 border-gray-200 hover:bg-gray-100 transition duration-200 ease-in-out">
             <img src="${item.category_icon}" alt="${item.category}" class="w-6 h-6 md:w-12 md:h-12">
             <p class="md:text-xl font-semibold">${item.category}</p>
             </a>
         `;
-        categoryBtnContainer.append(categoryContainer);
-    });
-}
+    categoryBtnContainer.append(categoryContainer);
+  });
+};
 // load Active Btn
 const loadActiveBtn = (categoryId) => {
-    console.log('category-'+categoryId);
-}
+  console.log("category-" + categoryId);
+};
 
 // load pet cards
 const loadPetCards = () => {
-    fetch(`https://openapi.programming-hero.com/api/peddy/pets`)
-    .then(response => response.json())
-    .then(data => {
-        // console.log(data.pets);
-        displayPetCards(data.pets);
+  fetch(`https://openapi.programming-hero.com/api/peddy/pets`)
+    .then((response) => response.json())
+    .then((data) => {
+      // console.log(data.pets);
+      displayPetCards(data.pets);
+      likeClick(data.pets);
     })
-    .catch(error => console.error('Error fetching data:', error));
-}
+    .catch((error) => console.error("Error fetching data:", error));
+};
 
 // display pet cards
 const displayPetCards = (pets) => {
-
-    const CardContainer = document.getElementById('card-container');
-    CardContainer.innerHTML = '';
-    pets.forEach(pet => {
-        const petCard = document.createElement('div');
-        petCard.innerHTML = `
+  const CardContainer = document.getElementById("card-container");
+  CardContainer.innerHTML = "";
+  pets.forEach((pet) => {
+    const petCard = document.createElement("div");
+    petCard.innerHTML = `
             <div class="card bg-base-100 shadow-sm">
                 <figure class="overflow-hidden h-48 shadow-lg">
                     <img src="${pet.image}" alt="${pet.category}" class="object-cover w-full h-full" />
@@ -70,7 +72,7 @@ const displayPetCards = (pets) => {
                         Price: ${pet.price}
                     </p>
                     <div class="card-actions justify-between">
-                        <button id="likeCard${pet.petId}" onclick="like(${pet.petId})" class="btn bg-white mt-2 md:px-4 px-6 py-2 rounded-lg">
+                        <button id="likeCard${pet.petId}" class="likeBtn btn bg-white mt-2 md:px-4 px-6 py-2 rounded-lg">
                                 <svg id="likeIcon${pet.petId}" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
                                 <path d="M5.5275 8.54163C6.19917 8.54163 6.805 8.16996 7.22 7.64163C7.86688 6.81631 8.67893 6.13511 9.60417 5.64163C10.2067 5.32163 10.7292 4.84496 10.9817 4.21246C11.159 3.76933 11.2501 3.29642 11.25 2.81913V2.29163C11.25 2.12587 11.3159 1.96689 11.4331 1.84968C11.5503 1.73247 11.7092 1.66663 11.875 1.66663C12.3723 1.66663 12.8492 1.86417 13.2008 2.2158C13.5525 2.56743 13.75 3.04435 13.75 3.54163C13.75 4.50163 13.5333 5.41079 13.1475 6.22329C12.9258 6.68829 13.2367 7.29163 13.7517 7.29163M13.7517 7.29163H16.3567C17.2117 7.29163 17.9775 7.86996 18.0683 8.72079C18.1058 9.07246 18.125 9.42913 18.125 9.79163C18.1284 12.0719 17.3492 14.2843 15.9175 16.0591C15.5942 16.4608 15.095 16.6666 14.58 16.6666H11.2333C10.8308 16.6666 10.43 16.6016 10.0475 16.475L7.4525 15.6083C7.07009 15.4811 6.66968 15.4164 6.26667 15.4166H4.92M13.7517 7.29163H11.875M4.92 15.4166C4.98917 15.5875 5.06417 15.7541 5.145 15.9183C5.30917 16.2516 5.08 16.6666 4.70917 16.6666H3.9525C3.21167 16.6666 2.525 16.235 2.30917 15.5266C2.02054 14.5793 1.87422 13.5944 1.875 12.6041C1.875 11.31 2.12084 10.0741 2.5675 8.93913C2.8225 8.29413 3.4725 7.91663 4.16667 7.91663H5.04417C5.4375 7.91663 5.665 8.37996 5.46084 8.71663C4.74908 9.88825 4.37369 11.2332 4.37584 12.6041C4.37584 13.5991 4.56917 14.5483 4.92084 15.4166H4.92Z" stroke="#131313" stroke-opacity="0.6" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
                             </svg>
@@ -81,17 +83,39 @@ const displayPetCards = (pets) => {
                 </div>
             </div>
         `;
-        CardContainer.appendChild(petCard);
+    CardContainer.appendChild(petCard);
+  });
+};
+
+const likeClick = (pets) => {
+  pets.forEach((pet) => {
+    const likeButton = document.getElementById(`likeCard${pet.petId}`);
+    likeButton.addEventListener("click", () => {
+      // change like icon color
+      const likeIcon = document.querySelector(`#likeIcon${pet.petId} path`);
+      likeIcon.classList.toggle("primary-stroke");
+      // console.log("like" + pet.petId);
+      // add pet to liked pets in favorite section
+      const noItems = document.getElementById("no-items");
+      noItems.classList.add("hidden");
+      const likedPets = document.getElementById("liked-pets");
+      console.log(likedPets);
+      likedPets.innerHTML += `
+        <figure class="overflow-hidden  rounded-lg">
+            <img src="${pet.image}" alt="${pet.category}" class="object-cover w-full h-full" />
+        </figure>
+        `;
     });
-}
+    const likeIcon = document.querySelector(`#likeIcon${pet.petId} path`);
+  });
+};
 
 // like
-const like=(petId)=>{
-    const likeIcon = document.querySelector(`#likeIcon${petId} path`);
-    likeIcon.classList.toggle("primary-stroke");
-    console.log('like' + petId);
-}
+// const like=(petId)=>{
+//     const likeIcon = document.querySelector(`#likeIcon${petId} path`);
+//     likeIcon.classList.toggle("primary-stroke");
+//     console.log('like' + petId);
+// }
 
 loadCategories();
 loadPetCards();
-// <img src="images/icons/Frame (4).svg" alt="icon" class="w-6 h-6">
