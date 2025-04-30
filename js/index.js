58a73bead19bcf3d477c94f938488b350e4638fe
Minhajh2o <h2o.minhaj@gmail.com>
2025-05-01 03:26:25 +0600

    small mistakes adjusted

--- js/index.js
+++ js/index.js
@@ -52,7 +52,7 @@ const displayPetCards = (pets) => {
                 </figure>
 
                 <div class="p-6 md:p-4 ">
-                    <h2 class="card-title">
+                    <h2 class="card-title mb-2">
                         ${pet.pet_name}
                     </h2>
                     <p class="flex items-center gap-2 text-gray-500 text-base">
@@ -72,13 +72,13 @@ const displayPetCards = (pets) => {
                         Price: ${pet.price}
                     </p>
                     <div class="card-actions justify-between">
-                        <button id="likeCard${pet.petId}" class="likeBtn btn bg-white mt-2 md:px-4 px-6 py-2 rounded-lg">
+                        <button id="likeCard${pet.petId}" class="likeBtn btn bg-white mt-3 md:px-4 px-6 py-2 rounded-lg">
                                 <svg id="likeIcon${pet.petId}" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
                                 <path d="M5.5275 8.54163C6.19917 8.54163 6.805 8.16996 7.22 7.64163C7.86688 6.81631 8.67893 6.13511 9.60417 5.64163C10.2067 5.32163 10.7292 4.84496 10.9817 4.21246C11.159 3.76933 11.2501 3.29642 11.25 2.81913V2.29163C11.25 2.12587 11.3159 1.96689 11.4331 1.84968C11.5503 1.73247 11.7092 1.66663 11.875 1.66663C12.3723 1.66663 12.8492 1.86417 13.2008 2.2158C13.5525 2.56743 13.75 3.04435 13.75 3.54163C13.75 4.50163 13.5333 5.41079 13.1475 6.22329C12.9258 6.68829 13.2367 7.29163 13.7517 7.29163M13.7517 7.29163H16.3567C17.2117 7.29163 17.9775 7.86996 18.0683 8.72079C18.1058 9.07246 18.125 9.42913 18.125 9.79163C18.1284 12.0719 17.3492 14.2843 15.9175 16.0591C15.5942 16.4608 15.095 16.6666 14.58 16.6666H11.2333C10.8308 16.6666 10.43 16.6016 10.0475 16.475L7.4525 15.6083C7.07009 15.4811 6.66968 15.4164 6.26667 15.4166H4.92M13.7517 7.29163H11.875M4.92 15.4166C4.98917 15.5875 5.06417 15.7541 5.145 15.9183C5.30917 16.2516 5.08 16.6666 4.70917 16.6666H3.9525C3.21167 16.6666 2.525 16.235 2.30917 15.5266C2.02054 14.5793 1.87422 13.5944 1.875 12.6041C1.875 11.31 2.12084 10.0741 2.5675 8.93913C2.8225 8.29413 3.4725 7.91663 4.16667 7.91663H5.04417C5.4375 7.91663 5.665 8.37996 5.46084 8.71663C4.74908 9.88825 4.37369 11.2332 4.37584 12.6041C4.37584 13.5991 4.56917 14.5483 4.92084 15.4166H4.92Z" stroke="#131313" stroke-opacity="0.6" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
                             </svg>
                         </button>
-                        <button class="btn primary-color bg-white mt-2 px-6 md:px-4 lg:px-5 py-2 rounded-lg">Adopt</button>
-                        <button onclick="loadPetDetails(${pet.petId})" class="btn primary-color bg-white mt-2 px-6 md:px-4 lg:px-5 py-2 rounded-lg">Details</button>
+                        <button class="btn primary-color bg-white mt-3 px-6 md:px-4 lg:px-5 py-2 rounded-lg">Adopt</button>
+                        <button onclick="loadPetDetails(${pet.petId})" class="btn primary-color bg-white mt-3 px-6 md:px-4 lg:px-5 py-2 rounded-lg">Details</button>
                     </div>
                 </div>
             </div>
@@ -153,7 +153,7 @@ const displayPetDetails = (pet) => {
                 <h2 class="card-title">
                             ${pet.pet_name}
                 </h2>
-                <div class=" grid grid-cols-2 gap-1 pt-4 pb-4 border-b border-gray-200">
+                <div class=" grid grid-cols-1 md:grid-cols-2 gap-1 pt-4 pb-4 border-b border-gray-200">
                     <p class="flex items-center gap-2 text-gray-500 text-base">
                         <img src="images/icons/Frame.svg" alt="icon" class="w-5 h-5">
                         Breed: ${pet.breed}
